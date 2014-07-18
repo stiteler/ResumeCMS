0b484d7537e05f109447e066b6aff8d0657cb340
Chris Stiteler <cstiteler@comcast.net>
2014-07-17 23:25:36 -0400

    trying new implementation with Candidates passed in Table events

--- src/view/TableEvent.java
+++ src/view/TableEvent.java
@@ -1,19 +1,21 @@
 package view;
 
 import java.util.EventObject;
+
+import Model.Candidate;
 /**
  * Class to hold data on Table events emitted by GUI
  * 
  * @author Chris Stiteler
- * 
- * TODO: Need to fix the problem of decoupling the TableEvent from the candidate
- * order that's displayed, essentially need to give table event info about the candidate? 
  *
  */
 public class TableEvent extends EventObject {
 	//methods: 1 is view resume, leaving open for additions later
 	private int method;
 	private int rowSelected;
+	
+	// Trying: CandidateID? 
+	private Candidate candidateSelected;
 	
 	/**
 	 * Constructor sets source (table) and row selected for
@@ -27,7 +29,23 @@ public class TableEvent extends EventObject {
 		this.rowSelected = rowSelected;
 	}
 	
+	/**
+	 * TableEvent constructor for when the even needs to know about the candidate
+	 * when the table model isn't linked directly to database model.. which shouldn't be coupled at all.
+	 * 
+	 * @param source Source table
+	 * @param candidateSelected Candidate that was selected in the table row
+	 */
+	public TableEvent(Object source, Candidate candidateSelected) {
+		super(source);
+		this.candidateSelected = candidateSelected;
+	}
+	
 	public int getRowSelected() {
 		return rowSelected;
+	}
+	
+	public Candidate getCandidateSelected() {
+		return this.candidateSelected;
 	}
 }
